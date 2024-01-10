e442bed2ac8f7ea9c33a8147ca93e178a2e5f5c6
kinumiya-norihito <99883931+kinumiya-norihito@users.noreply.github.com>
2024-01-10 15:37:53 +0900

    ap実装(発信なし)

--- server/app/routers/api/activitypub.py
+++ server/app/routers/api/activitypub.py
@@ -5,6 +5,7 @@ from crud import SessionCrud
 from crud.schemas import ActorSessionSchema
 from db import schemas
 from db.crud import actor as actor_crud
+from db.crud import note as note_crud
 from db.session import get_db
 
 # define router
@@ -12,34 +13,56 @@ router = APIRouter(
     tags=["ActivityPub関連"]
 )
 
+# ここを変更
+host = "127.0.0.1:58080:80"
 
-@router.post("/", description="Actor作成")
-def create(
-        response: Response,
-        data: schemas.ActorCreate,
-        db: Session = Depends(get_db)
-) -> schemas.ActorPublic | schemas.ErrorResponse:
-    actor = actor_crud.get_by_name(db, data.name)
-    if actor is not None:
-        response.status_code = 409
-        return schemas.ErrorResponse(message="Name already exists")
-
-    actor = actor_crud.create(db, data)
-
-    with SessionCrud() as session_crud:
-        session_crud.create(response, ActorSessionSchema(actor_id=actor.id))
-
-    return actor
-
-
-@router.get("/", description="ログイン中Actor取得")
+# /api/activitypub/users/{user_name} : get
+@router.get("/users/{user_name}", description="[Actor]エンドポイント")
 async def get(
-        request: Request,
+        response: Response,
+        user_name: str,
         db: Session = Depends(get_db)
-) -> schemas.ActorPublic | None:
-    with SessionCrud() as session_crud:
-        session: ActorSessionSchema = session_crud.get(request)
-    if session is None:
-        return None
+) :
+    # actorを取得
+    actor = actor_crud.get_by_name(db, user_name)
+    if actor is None:
+        response.status_code = 404
+        return schemas.ErrorResponse(message="User not found")
 
-    return actor_crud.get(db, session.actor_id)
+    activitypub_actor = {
+        "@context": ["https://www.w3.org/ns/activitystreams"],
+        "type": "Person",
+        "id": f"https://{host}/api/activitypub/users/{actor.name}",
+        "inbox": f"https://{host}/api/activitypub/users/{actor.name}/inbox",
+ 	    "outbox": f"https://{host}/api/activitypub/users/{actor.name}/outbox",
+        "preferredUsername": f"{actor.name}",
+        "discoverable": True
+    }
+    return activitypub_actor
+
+# /api/activitypub/notes/{note_id} : get
+@router.get("/notes/{note_id}", description="[Note]エンドポイント")
+async def get(
+        response: Response,
+        note_id: int,
+        db: Session = Depends(get_db)
+) :
+    # actorを取得
+    note = note_crud.get(db, note_id)
+    if note is None:
+        response.status_code = 404
+        return schemas.ErrorResponse(message="Note not found")
+
+    # ! urlを書き換える
+    #   - id
+    #   - inbox
+    #   - outbox
+    activitypub_note = {
+        "@context": ["https://www.w3.org/ns/activitystreams"],
+        "type": "Note",
+        "id": "https://{host}/api/activitypub/notes/{note.id}",        
+        "content": note.content,
+        "attributedTo": "https://{host}/api/activitypub/users/{note.actor_id}",
+        "published": note.created_at
+    }
+    return activitypub_note
