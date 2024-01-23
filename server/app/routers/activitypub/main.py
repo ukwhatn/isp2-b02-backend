450d24eed031f71580f75214f098f5f045727e44
Yuki Watanabe <ukwhatn@gmail.com>
2024-01-23 23:50:05 +0900

    wip

--- server/app/routers/activitypub/main.py
+++ server/app/routers/activitypub/main.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter, Request, Response, Depends
 import json
+from sqlalchemy.orm import Session
+
+from crud import SessionCrud
+from crud.schemas import ActorSessionSchema
+from db import schemas
+from db.crud import actor as actor_crud
+from db.session import get_db
 
 from fastapi import APIRouter
 
@@ -45,7 +52,7 @@ def node_info_2_1():
 
 
 @router.get("/.well-known/webfinger")
-def well_known_webfinger(resource: str, response: Response):
+def well_known_webfinger(resource: str):
     # :と@で挟まれた文字列を取得
     user_name = resource.split(":")[1].split("@")[0]
 
@@ -61,3 +68,25 @@ def well_known_webfinger(resource: str, response: Response):
     }
 
     return Response(content=json.dumps(data), media_type="application/jrd+json")
+
+
+@router.get("/actor/{user_name}")
+def actor(user_name: str, db: Session = Depends(get_db)):
+    user = actor_crud.get_by_name(db, user_name)
+    if user is None:
+        return Response(status_code=404)
+
+    data = {
+        "@context": [
+            "https://www.w3.org/ns/activitystreams",
+            "https://w3id.org/security/v1"
+        ],
+        "id": f"https://b02.isp2.ukwhatn.com/actor/{user.name}",
+        "type": "Person",
+        "preferredUsername": user.name,
+        "inbox": f"https://b02.isp2.ukwhatn.com/actor/{user.name}/inbox",
+        "outbox": f"https://b02.isp2.ukwhatn.com/actor/{user.name}/outbox",
+        "discoverable": True
+    }
+
+    return Response(content=json.dumps(data), media_type="application/activity+json")
